5368854e5387d393aa0ec93fbf4a8a07fcf35fec
Djello Nicolas <djellodesignerweb@gmail.com>
2024-10-11 20:03:58 -0300

    design-interiores
    design

--- designer.js
+++ designer.js
@@ -3,7 +3,7 @@
 
 
 //Aqui adicionamos um evento de escutar o click do Mouse sobre o ícone de Hamburguer.
-hamburgerButton.addEventListener("click", function() {
+hamburguerButton.addEventListener("click", function() {
     mobileMenu.classList.add("flex");
     //Criamos uma função que a ouvir o click do Mouse, ele adicione uma Class, que está fazendo nossa caixa (Div) aparecer, ou seja, ficar visível.
 });
